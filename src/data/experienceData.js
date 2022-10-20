5d0b586c380502952185b716115eae8c3fe27a8c
Dennis0Goldberg <dennisg@bridgify.io>
2022-10-20 11:08:02 +0300

    experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -6,18 +6,5 @@ export const experienceData = [
         startYear: 'June 2022',
         endYear: "current"
     },
-    // {
-    //     id: 2,
-    //     company: 'Vehement Capital Partners',
-    //     jobtitle: 'Backend Developer',
-    //     startYear: '2019',
-    //     endYear: '2020'
-    // },
-    // {
-    //     id: 3,
-    //     company: 'Wonka Industries',
-    //     jobtitle: 'Marketing',
-    //     startYear: '2020',
-    //     endYear: 'Present'
-    // },
+  
 ]
