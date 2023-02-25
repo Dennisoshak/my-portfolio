83721ba9e0f2057658eb265d37ae854343871337
Dennis0Goldberg <dennisg@bridgify.io>
2023-02-25 15:20:37 +0200

    Updates

--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-  email: "dennisoshak@gmail.com",
-  phone: "+972525529843",
-  address: "Alterman 23, Tel Aviv, Israel ",
-
-  sheetAPI: "https://sheetdb.io/api/v1/fyjh5iropv7uv",
-};
