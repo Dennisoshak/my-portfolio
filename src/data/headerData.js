2e3d4dbfdd13c0ee0b8be8d111a85b2fa18a5391
DennisG <Dennisoshak@gmail.com>
2025-08-25 15:45:46 +0300

    new resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,5 +1,5 @@
 import profileImage from "../assets/png/Dennis.png";
-import resume from "../assets/pdf/Denis_Goldberg_CV.pdf";
+import resume from "../assets/pdf/CV Dennis Goldberg.pdf";
 
 export const headerData = {
   name: "Dennis Goldberg",
